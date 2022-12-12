266620a7cfff9845a945c09a4b74779e88db5884
slahser0713 <wenyingkai@gmail.com>
2022-12-13 01:45:23 +0800

    获取局域网ip

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -27,43 +28,81 @@ type Json struct {
 	Raw string `json:"raw"`
 }
 
-func main() {
-	var TextsController func(*gin.Context)
-	// 获取文本并返回文件路径
-	TextsController = func(c *gin.Context) {
-		var json Json
-		// 获取网页端输入文本(post请求的body)
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			exe, err := os.Executable() // 获取当前可执行文件的路径
-			if err != nil {
-				log.Fatal(err)
+func AddressesController(c *gin.Context) {
+	addrs, _ := net.InterfaceAddrs()
+	result := []string{}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				result = append(result, ipnet.IP.String())
 			}
-			dir := filepath.Dir(exe) // 获取当前执行文件的目录
-			if err != nil {
-				log.Fatal(err)
-			}
-			filename := uuid.New().String()          // 生成一个文件名
-			uploads := filepath.Join(dir, "uploads") // 拼接 uploads 的绝对路径
-			err = os.MkdirAll(uploads, os.ModePerm)  // 创建 uploads 目录
-			if err != nil {
-				log.Fatal(err)
-			}
-			fullpath := path.Join("uploads", filename+".txt")                            // 拼接文件的绝对路径（不含 exe 所在目录）
-			err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 将 json.Raw 写入文件
-			if err != nil {
-				log.Fatal(err)
-			}
-			c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) // 返回文件的绝对路径（不含 exe 所在目录）
 		}
 	}
+	// 转为json，通过http响应前端
+	c.JSON(http.StatusOK, gin.H{"addresses": result})
+}
 
+func GetUploadsDir() (uploads string) {
+	exe, err := os.Executable() // 获取当前可执行文件的路径
+	if err != nil {
+		log.Fatal(err) // 错误打印
+	}
+	dir := filepath.Dir(exe)
+	uploads = filepath.Join(dir, "uploads")
+	return
+}
+
+// 读取本地路径，写入HTTP相应
+func UploadsController(c *gin.Context) {
+	if path := c.Param("path"); path != "" {
+		target := filepath.Join(GetUploadsDir(), path)
+		c.Header("Content-Description", "File Transfer")
+		c.Header("Content-Transfer-Encoding", "binary")
+		c.Header("Content-Disposition", "attachment; filename="+path)
+		c.Header("Content-Type", "application/octet-stream")
+		c.File(target)
+	} else {
+		c.Status(http.StatusNotFound)
+	}
+}
+
+// 获取文本并返回文件路径
+func TextsController(c *gin.Context) {
+	var json Json
+	// 获取网页端输入文本(post请求的body)
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() // 获取当前可执行文件的路径
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe)                 // 获取当前执行文件的目录
+		filename := uuid.New().String()          // 生成一个文件名
+		uploads := filepath.Join(dir, "uploads") // 拼接 uploads 的绝对路径
+		err = os.MkdirAll(uploads, os.ModePerm)  // 创建 uploads 目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")                            // 拼接文件的绝对路径（不含 exe 所在目录）
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 将 json.Raw 写入文件
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) // 返回文件的绝对路径（不含 exe 所在目录）
+	}
+}
+
+func main() {
+	// 主线程休眠后进入该协程
 	go func() { // gin 协程
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
 		router.POST("/api/v1/texts", TextsController)
+		router.GET("/api/v1/addresses", AddressesController)
+		router.GET("/uploads/:path", UploadsController)
 		router.StaticFS("/static", http.FS(staticFiles))
 		router.NoRoute(func(c *gin.Context) {
 			// 访问路径
